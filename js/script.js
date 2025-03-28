1e4ae141be57625d2ab1bd3ea4c1341f98029cbe
javier <javier_2790@outlook.com>
2025-03-28 17:40:07 +0100

    feat: web finish, just fixing some details

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//script para activar el menu hamburguesa
 document.addEventListener("DOMContentLoaded", function () {
     const menuBtn = document.querySelector("#menu-btn");
     const menu = document.querySelector("#menu");
@@ -10,3 +11,22 @@ document.addEventListener("DOMContentLoaded", function () {
         console.error("Elemento no encontrado en el DOM");
     }
 });
+
+
+// Script de validacion de boostrap
+(() => {
+    'use strict'
+  
+    const forms = document.querySelectorAll('.needs-validation')
+  
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
