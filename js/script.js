4c2ffc23df51e0bb009528dae44a835a208fc92c
javier <javier_2790@outlook.com>
2025-05-11 19:24:32 +0200

    feat: finis point 2

--- js/script.js
+++ js/script.js
@@ -46,4 +46,17 @@ document.addEventListener("DOMContentLoaded", function () {
       icon.classList.add("lni-play");
     }
   }
+
+//Script para la animacion
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    } else {
+      entry.target.classList.remove('visible');
+    }
+  });
+});
+document.querySelectorAll('.quienes-somos').forEach(el => observer.observe(el));
   
