31f09322e86639239f976919b956273ccb4db028
javier <javier_2790@outlook.com>
2025-05-15 22:19:44 +0200

    fix: improve the cards with boostrap

--- js/script.js
+++ js/script.js
@@ -31,22 +31,6 @@ document.addEventListener("DOMContentLoaded", function () {
     })
   })()
 
- 
-  function togglePlay() {
-    const video = document.getElementById("myVideo");
-    const icon = document.getElementById("playIcon");
-    
-    if (video.paused) {
-      video.play();
-      icon.classList.remove("lni-play");
-      icon.classList.add("lni-pause");
-    } else {
-      video.pause();
-      icon.classList.remove("lni-pause");
-      icon.classList.add("lni-play");
-    }
-  }
-
 //Script para la animacion
 
 const observer = new IntersectionObserver((entries) => {
